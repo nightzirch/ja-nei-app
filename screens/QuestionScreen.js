c4770283818fc514ad4f97b52aa9dd518f1896b2
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2023-06-26 15:01:02 +0200

    New answers

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 999,
+    borderRadius: "100%",
     height: 50,
     width: 50,
     zIndex: 999,
